7658dc5862c8558a5ed81b5a30c88414de9ecb84
Delmar <reliablesource13@gmail.com>
2023-03-28 15:19:51 -0700

    Made the schema

--- schema.js
+++ schema.js
@@ -1,1 +1,13 @@
-//Database Schema
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fName: String,
+    lName: String,
+    email: String,
+    campus: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+exports.User = User;
