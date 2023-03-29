0560df1cf911c122329cdae0f60e9bc3494d85ef
Delmar <reliablesource13@gmail.com>
2023-03-29 14:16:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const os = require('os');
 
 const app = express();
 const port = 8080;
@@ -9,5 +10,7 @@ app.use(bodyparser.json());
 app.get('/api', (req, res) => {
     res.send({ msg: 'success' });
 });
+
+console.log(os.networkInterfaces());
 
 app.listen(port, console.log(`Listening on port ${port}`));
