6afca2b1a1e8840a8b6e106f11e7984604e8c33f
Delmar <reliablesource13@gmail.com>
2023-03-29 13:57:33 -0700

    Simple server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-//Server Stuff
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+const port = 8080;
+
+app.use(bodyparser.json());
+
+app.get('/', (req, res) => {
+    res.send({ msg: 'success' });
+});
+
+app.listen(port, console.log(`Listening on port ${port}`));
