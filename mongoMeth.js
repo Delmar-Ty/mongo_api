44b3611cb174e30ac654162dc484aea3b45133cc
Delmar-Ty <89948545+Delmar-Ty@users.noreply.github.com>
2023-03-29 17:42:43 -0700

    Mongo Stuff

--- mongoMeth.js
+++ mongoMeth.js
@@ -5,26 +5,20 @@ const dbURL = 'mongodb+srv://Delmar:Something123@cluster0.tg0mxdx.mongodb.net/te
 const db = {
     createUser: (data) => {
         try {
-            mongoose.connect(dbURL, () => {
-                if (error) throw error;
-                const doc = new User({
-                    fName: 'Jim',
-                    lName: 'Slim',
-                    email: data.email,
-                    campus: 'Central',
-                    password: data.password
-                });
+            const doc = new User({
+                fName: 'Jim',
+                lName: 'Slim',
+                email: data.email,
+                campus: 'Central',
+                password: data.password
+            });
 
-                doc.save(error => {
-                    if (error) throw error;
-                });
+            doc.save(error => {
+                if (error) throw error;
             });
         } catch (error) {
             console.log(error);
         }
-    },
-    disconnect: () => {
-        mongoose.connection.close();
     }
 };
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const mongoMeth = require('./mongoMeth');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
@@ -14,9 +15,11 @@ app.get('/api', (req, res) => {
     res.send({ msg: 'success' });
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     console.log(req.body.email, req.body.password);
+    await mongoose.connect();
     db.createUser({ email: req.body.email, password: req.body.password });
+    mongoose.disconnect();
 });
 
 app.listen(port, console.log(`Listening on port ${port}`));
