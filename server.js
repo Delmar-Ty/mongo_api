b2652640a1e7b6245d01d19913cc06f74cddbc0b
Delmar-Ty <89948545+Delmar-Ty@users.noreply.github.com>
2023-04-01 17:28:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const mongoMeth = require('./mongoMeth');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 const port = 8080;
 const db = mongoMeth.db;
-const dbURL = 'mongodb+srv://Delmar:Something123@cluster0.tg0mxdx.mongodb.net/User';
-
 
 app.use(bodyparser.json());
 app.use(cors());
@@ -23,15 +20,15 @@ app.post('/login', async (req, res) => {
     if (result.exists && !result.logged) {
         const user = await db.getUser(result.id);
         if (user.email === data.email && user.password === data.password) {
-            res(JSON.stringify({ auth: true, msg: 'Success' }));
+            res.send(JSON.stringify({ auth: true, msg: 'Success' }));
             db.updateLogin(result.id, JSON.stringify({logged: true, device: data.device}));
         } else {
-            res(JSON.stringify({ auth: false, msg: 'Invalid login' }));
+            res.send(JSON.stringify({ auth: false, msg: 'Invalid login' }));
         }
     } else if (result.exists && result.logged) {
-        res(JSON.stringify({ auth: false, msg: 'User is already logged in on a different device' }));
+        res.send(JSON.stringify({ auth: false, msg: 'User is already logged in on a different device' }));
     } else {
-        res(JSON.stringify({ auth: false, msg: 'Invalid login' }));
+        res.send(JSON.stringify({ auth: false, msg: 'Invalid login' }));
     }
 });
 
